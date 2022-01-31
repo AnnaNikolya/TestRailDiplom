7628410896d18280cec42a23a0cb78e6b8e35bf0
AnnaNikolya <90003547+AnnaNikolya@users.noreply.github.com>
2022-01-31 23:26:53 +0300

    Add maven depenency CircleCI

--- src/test/java/tests/SignUpTest.java
+++ src/test/java/tests/SignUpTest.java
@@ -4,7 +4,6 @@ import io.qameta.allure.Description;
 import lombok.extern.log4j.Log4j2;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import static pages.SignUpPage.openHomePage;
 
 @Log4j2
@@ -13,7 +12,7 @@ public class SignUpTest extends BaseTest {
     @Description("Create account")
     public void openHomePageTest() {
         openHomePage();
-        signUpPage.enterValue("diplontestwork41252", "YourFistName11", "YourLastName11",
+        signUpPage.enterValue("diplontestwork41d252", "YourFistName11", "YourLastName11",
                 "BY", "1", "diplontestrail@mailinator.com", "kkk34",
                 "Please confirm your account by clicking the link we've just emailed you");
         String informText = "Please confirm your account by clicking the link we've just emailed you";
