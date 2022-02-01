60463ed0af9716dca2c3553a45bda3bd22f99b71
AnnaNikolya <90003547+AnnaNikolya@users.noreply.github.com>
2022-02-01 19:14:25 +0300

    Add maven depenency CircleCI

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.safari.SafariDriver;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
 import org.testng.annotations.Listeners;
 import pages.*;
 
